5c64b4d6446c299fe730c4b84054272fa20e2c55
NiloyKumarKundu <niloykk.connect@gmail.com>
2025-10-16 18:26:45 +0600

    prompt updated

---
+++ prompts.py
@@ -0,0 +1,11 @@
+prompts = """
+
+I want to update the spreadsheet of id 1paCuL1BzJv8UadKdIjZl9Lee8BlECBGQyCDJOuEb-8k in the sheet "data" in the cell A5 to Hujaifa.
+
+What is our company's vacation policy?
+
+Add an expense - Last day, I buy a burger from Burger King, which cost me 60 USD.
+
+Show me all the expenses I have made in the last 30 days.
+
+"""
